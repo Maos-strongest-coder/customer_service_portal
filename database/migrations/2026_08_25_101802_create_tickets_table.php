2c3952655c4cc9363ef5e171e554275cdd84499c
Ramazan <rbugday98@gmail.com>
2026-08-25 12:57:44 +0200

    defined table layout

---
+++ database/migrations/2026_08_25_101802_create_tickets_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tickets', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('category');
+            $table->enum('status', ['open', 'started', 'closed'])->default('open');
+            $table->foreignId('issued_by')->constrained('users')->onDelete('cascade');
+            $table->foreignId('issued_to')->nullable()->constrained('users')->onDelete('cascade');
+            $table->timestamps();
+            $table->index(['issued_by', 'created_at']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tickets');
+    }
+};
