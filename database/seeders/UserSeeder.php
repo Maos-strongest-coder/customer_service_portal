dce0d5d18c20528ab1dc12ac19b28f5b2fbb925c
Ramazan Bugday <rbugday98@gmail.com>
2026-08-27 13:33:59 +0200

    user test account seed

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -19,15 +19,21 @@ class UserSeeder extends Seeder
             ->admin()
             ->create();
 
-        User::factory()
-            ->count(15)
-            ->user()
-            ->create();
-            
         User::factory()->admin()->create([
             'first_name' => 'Admin',
             'last_name' => 'Tester',
             'email' => 'admin@test.com',
+        ]);
+
+        User::factory()
+            ->count(15)
+            ->user()
+            ->create();
+
+        User::factory()->user()->create([
+            'first_name' => 'User',
+            'last_name' => 'Tester',
+            'email' => 'user@test.com',
         ]);
     }
 }
