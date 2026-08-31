427e4dc6a9585cf4b3a45a6d7c55123c2f5fd838
maos_strongest_coder <rbugday98@gmail.com>
2026-08-31 15:13:18 +0200

    specified errors for login page

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -24,7 +24,8 @@ class LoginController extends Controller
         return response()->json([
             'message' => 'The provided credentials do not match our records.',
             'errors' => [
-                'email' => ['The provided credentials do not match our records.'],
+                'email' => ['The provided email does not match our records.'],
+                'password' => ['The provided password does not match our records.'],
             ],
         ], 422);
     }
