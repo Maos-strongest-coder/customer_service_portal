ca2e61ddee1d49c3f17d55e3cb23257d85606230
maos_strongest_coder <rbugday98@gmail.com>
2026-08-31 15:16:31 +0200

    fixed factory

--- database/factories/TicketFactory.php
+++ database/factories/TicketFactory.php
@@ -24,6 +24,8 @@ class TicketFactory extends Factory
             'status' => fake()->randomElement(['open', 'started', 'closed']),
             'issued_by' => User::factory()->user(),
             'issued_to' => User::factory()->admin(),
+            'created_at' => fake()->dateTimeBetween('-2 month', '-1 month'),
+            'updated_at' => fake()->dateTimeBetween('-1 month')
         ];
     }
 }
