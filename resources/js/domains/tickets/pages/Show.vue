ed5050ddf5fc4f42d97c304146bdef2a0dd7d078
Ramazan <rbugday98@gmail.com>
2026-09-01 12:03:59 +0200

    added handle submit and key for  reply list

--- resources/js/domains/tickets/pages/Show.vue
+++ resources/js/domains/tickets/pages/Show.vue
@@ -1,7 +1,7 @@
 <template>
     <div v-if="ticket">
         <h2>ticket #{{ ticket?.id }}</h2>
-        
+
         <table>
             <thead>
                 <tr>
@@ -16,57 +16,61 @@
                 </tr>
             </thead>
             <tbody>
-                <TicketCard :ticket="ticket"/>
+                <TicketCard :ticket="ticket" />
             </tbody>
         </table>
-        
+
         <h2>Replies</h2>
 
         <div v-if="ticket.replies && ticket.replies.length > 0">
-            <div v-for="reply in ticket.replies">
-                <strong>{{ reply.user?.full_name  }}</strong>
+            <div v-for="reply in ticket.replies " :key="reply.id">
+                <strong>{{ reply.user?.full_name }}</strong>
                 |
                 <span>{{ reply.created_at }}</span>
-        
+
                 <p>{{ reply.message }}</p>
             </div>
-
         </div>
 
-        <div v-else>
-            No replies yet.
-        </div>
-    </div>
-       
-    <div>
+        <div v-else>No replies yet.</div>
 
+        <ChatBox :ticketId="ticket.id" @submit="handleSubmit"/>
     </div>
+
+    
 </template>
 
 <script setup>
-import { computed, onMounted, ref } from 'vue';
-import { ticketStore } from '../store';
-import { Navigation } from '../../../facades/router';
+import {computed, onMounted, ref} from 'vue';
+import {ticketStore} from '../store';
+import {Navigation} from '../../../facades/router';
 import TicketCard from '../components/TicketCard.vue';
+import ChatBox from '../components/ChatBox.vue';
+import { Http } from '../../../facades/http';
 
-const currentId = ref(null)
+const currentId = ref(null);
 
 const ticket = computed(() => {
     if (!currentId.value) return null;
     return ticketStore.getters.getById(currentId.value).value;
 });
-   
 
-onMounted( async () => {
-        const currentRoute = Navigation.currentRoute();
+onMounted(async () => {
+    const currentRoute = Navigation.currentRoute();
 
-        const ticketId = currentRoute.params.id;
+    const ticketId = currentRoute.params.id;
 
-        if (ticketId) {
-            currentId.value = ticketId;
-            await ticketStore.actions.getOne({id :ticketId})
-        }
-    })
+    if (ticketId) {
+        currentId.value = ticketId;
+        await ticketStore.actions.getOne({id: ticketId});
+    }
+});
 
+const handleSubmit = async (formData) => {
+    await Http.post(`/tickets/${formData.ticketId}/replies`, {
+        message: formData.message
+    });
 
+    await ticketStore.actions.getOne({ id: formData.ticketId})
+}
 </script>
