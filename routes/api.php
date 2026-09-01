d70926e099e5a1fca8bc37998ab02c8a6a3fb948
Ramazan <rbugday98@gmail.com>
2026-09-01 12:04:37 +0200

    added API route for storing replies

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\TicketController;
+use App\Http\Controllers\TicketReplyController;
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('/me', [AuthController::class, 'me']);
@@ -12,6 +13,8 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::get('/tickets', [TicketController::class, 'index']);
 
     Route::get('/tickets/{ticket}', [TicketController::class, 'show']);
+
+    Route::post('/tickets/{ticket}/replies', [TicketReplyController::class, 'store']);
 
     Route::post('/logout', [LoginController::class, 'logout']);
 });
