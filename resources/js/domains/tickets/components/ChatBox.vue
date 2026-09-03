94c27a3457038291051852bd725375c28046e68a
Ramazan Bugday <rbugday98@gmail.com>
2026-09-03 14:49:38 +0200

    chatbox now works on reply as well as note

--- resources/js/domains/tickets/components/ChatBox.vue
+++ resources/js/domains/tickets/components/ChatBox.vue
@@ -1,43 +1,47 @@
 <template>
     <div class="chat-container">
         <form class="chat-form" @submit.prevent="handleSubmit">
-            <input v-model="form.message" placeholder="Write a reply here" class="chat-input" required />
+            <input
+                v-model="form.message"
+                :placeholder="type === 'note' ? 'Write an internal note here...' : 'Write a reply here...'"
+                class="chat-input"
+                required
+            />
 
-            <button type="submit" class="chat-submit">Send</button>
+            <button type="submit" class="chat-submit">
+                {{ type === 'note' ? 'Add Note' : 'Send' }}
+            </button>
         </form>
     </div>
 </template>
 
 <script setup>
-import {ref, onMounted} from 'vue';
-import {authStore} from '../../Auth/store';
+import {ref} from 'vue';
 
 const props = defineProps({
     ticketId: {
         type: [String, Number],
         required: true,
     },
+    type: {
+        type: String,
+        default: 'reply',
+    },
 });
 
 const emit = defineEmits(['submit']);
 
 const form = ref({
-    userId: null,
     ticketId: props.ticketId,
     message: '',
 });
 
-onMounted(async () => {
-    const user = await authStore.actions.me();
-
-    if (user && user.id) {
-        form.value.userId = user.id;
-    }
-});
-
 const handleSubmit = () => {
-    emit('submit', {...form.value});
+    emit('submit', {
+        ...form.value,
+        type: props.type,
+    });
 
     form.value.message = '';
-}
+};
 </script>
