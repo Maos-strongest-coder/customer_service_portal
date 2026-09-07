9f90d8cd624eeca49bf0972a860be9bf8e49e694
maos_strongest_coder <rbugday98@gmail.com>
2026-09-07 13:56:25 +0200

    added edit route

--- resources/js/domains/tickets/routes.ts
+++ resources/js/domains/tickets/routes.ts
@@ -1,10 +1,12 @@
 import Overview from './pages/Overview.vue';
 import Show from './pages/Show.vue';
 import Create from './pages/Create.vue';
+import Edit from './pages/Edit.vue';
 
 export const ticketRoutes = [
     {path: '/tickets', component: Overview, name: 'overview'},
     {path: '/tickets/create', component: Create, name: 'create'},
+    {path: '/tickets/:id/edit', component: Edit, name: 'edit'},
     {path: '/tickets/:id', component: Show, name: 'show'},
     {path: '/tickets/:id/notes', component: Show, name: 'notes'},
 ];
