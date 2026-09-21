9e265d8956ab444a51b065a40286f4fcb0bc16e5
maos_strongest_coder <rbugday98@gmail.com>
2026-09-21 14:53:52 +0200

    set up category crud

---
+++ resources/js/domains/categories/pages/Overview.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import {ref} from 'vue'
+import Table from '../components/Table.vue';
+import ChatBox from '../../components/ChatBox.vue';
+import { isAdmin } from '../../Auth/store';
+
+const tableRef = ref(null);
+
+const handleAddCategory = async name => {
+    await tableRef.value.addCategory(name)
+};
+</script>
+
+<template>
+    <Table ref="tableRef" />
+    <ChatBox v-if="isAdmin" placeholder="New category name..." buttonLabel="Add Category" @submit="handleAddCategory" />
+</template>
